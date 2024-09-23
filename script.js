9b839e373cc06434bec64f1995ee9f4c5dc7a928
natesh1999 <nateshgowda.1999@gmail.com>
2024-09-23 22:17:26 +0530

    fetching data using API

---
+++ script.js
@@ -0,0 +1,28 @@
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=c43ea5b05e010667596d1b90d5bd149f&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280/'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?&api_key=c43ea5b05e010667596d1b90d5bd149f&query=" '
+
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+
+//get initial movies
+// getMovies(API_URL)
+
+async function getMovies(url) {
+    const res = await fetch(url)
+    const data = await res.json()
+
+    console.log(data.results)
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const searchTerm = search.value
+
+    if(searchTerm && searchTerm !== '') {
+        getMovies(SEARCH_API + searchTerm)
+    }else{
+        window.location.reload()
+    }
+})
